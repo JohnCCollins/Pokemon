54499b757ec98e714457941b825b9fd169f2e337
JohnCCollins <jcc2292+1@gmail.com>
2021-03-29 18:22:20 -0400

    * Pokemon.sln: First Commit
    * data.txt:
    * Program.cs:
    * Pokemon.csv:
    * Pokemon.csproj:

---
+++ Pokemon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "Pokemon.csv");
+            var fileContents = ReadFile(fileName);
+            Console.WriteLine(fileContents);
+
+            Console.ReadLine();
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+            
+        }
+        
+    }
+    
+}
