c0c92217b51ed83dc909a8570b9a01ddaffedd35
JohnCCollins <jcc2292+1@gmail.com>
2021-03-30 00:14:44 -0400

    Display name of searched Pokemon

---
+++ Pokemon/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Pokemon
+{
+    public class Pokemon
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Type1 { get; set; }
+        public string Type2 { get; set; }
+        public int Hp { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpAttack { get; set; }
+        public int SpDefense { get; set; }
+        public int Speed { get; set; }
+        public int Generation { get; set; }
+        public string Legendary { get; set; }
+
+    }
+}
