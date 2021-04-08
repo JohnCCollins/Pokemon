dc24a3af429e103e7862d9d7a09b653899080c3c
JohnCCollins <jcc2292+1@gmail.com>
2021-04-08 01:52:06 -0400

    Functioning Battle System (basic version)

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -9,6 +9,13 @@ namespace Pokemon
 {
     class Program
     {
+
+        static Pokemon[] team = new Pokemon[3];
+        static Pokemon[] enemyTeam = new Pokemon[3];
+
+        static Pokemon[] fighters = new Pokemon[2];
+
+        static Random enemySel = new Random();
         static void Main(string[] args)
         {
             string currentDirectory = Directory.GetCurrentDirectory();
@@ -16,8 +23,7 @@ namespace Pokemon
             var fileName = Path.Combine(directory.FullName, "Pokemon.csv");
             var fileContents = ReadPokemon(fileName);
 
-            Pokemon[] team = new Pokemon[3];
-            Pokemon[] enemyTeam = new Pokemon[3];
+
 
             Console.WriteLine("Please type in three pokemon.");
 
@@ -26,224 +32,201 @@ namespace Pokemon
             sel[1] = Console.ReadLine();
             sel[2] = Console.ReadLine();
 
-            Random enemySel = new Random();
 
-            for (int i = 0; i < sel.Length; i++)
+
+            for (int i = 0; i <= team.Length-1; i++)
             {
-                int foe = enemySel.Next(1, 150);
+
                 foreach (Pokemon pokemon in fileContents)
                 {
-                    if (pokemon.Number == foe)
-                    {
-                        enemyTeam[i] = pokemon;
-                        Console.WriteLine(pokemon.Name + " added by opponent.");
-                    }
+
                     if (pokemon.Name == sel[i])
                     {
-                        Console.WriteLine(pokemon.Name + " added!");
                         team[i] = pokemon;
+                        Console.WriteLine(pokemon.Name + " added!");
+                        
                     }
                 }
             }
+
+            for (int i = 0; i <= enemyTeam.Length-1; i++)
+                foreach (Pokemon pokemon in fileContents)
+                {
+                    int foe = enemySel.Next(1, 150);
+                    if (pokemon.Number == foe)
+                    {
+                        enemyTeam[i] = pokemon;
+                        Console.WriteLine(enemyTeam[i].Name + " added by opponent.");
+                    }
+                }
 
             Console.WriteLine("Your team is: " + team[0].Name + ", " + team[1].Name + ", " + team[2].Name);
             Console.WriteLine("The opponent's team is: " + enemyTeam[0].Name + ", " + enemyTeam[1].Name + ", " + enemyTeam[2].Name);
 
-            Console.WriteLine("Choose which pokemon you will use first.");
-            Console.WriteLine("Press 1 to send out " + team[0].Name);
-            Console.WriteLine("Press 2 to send out " + team[1].Name);
-            Console.WriteLine("Press 3 to send out " + team[2].Name);
-            var choice = Console.ReadKey();
-            Pokemon fighter = new Pokemon();
+            chooseFighters();
+            Battle();
+        }
 
-            while (choice.Key != ConsoleKey.D1 && choice.Key != ConsoleKey.D2 && choice.Key != ConsoleKey.D3)
+        public static void teamSelection()
+        {
+
+        }
+
+        public static void chooseFighters()
+        {
+
+            Console.WriteLine("Which pokemon will you send out?");
+
+            var choice = Console.ReadLine();
+            for (int i = 0; i <= team.Length-1; i++)
             {
-                if (choice.Key == ConsoleKey.D1)
+                if (choice == team[i].Name)
                 {
-                    fighter = team[0];
-                    Console.WriteLine();
-                    Console.WriteLine("Sent out " + team[0].Name);
-                }
-                else if (choice.Key == ConsoleKey.D2)
-                {
-                    fighter = team[1];
-                    Console.WriteLine();
-                    Console.WriteLine("Sent out " + team[1].Name);
-                }
-                else if (choice.Key == ConsoleKey.D3)
-                {
-                    fighter = team[2];
-                    Console.WriteLine();
-                    Console.WriteLine("Sent out " + team[2].Name);
+                    fighters[0] = team[i];
+                    Console.WriteLine("Sent out " + fighters[0].Name);
                 }
             }
 
-            int foeChoice = enemySel.Next(1, enemyTeam.Length);
-            Pokemon foeFighter= new Pokemon();
-            if (foeChoice == 1)
+            int foeChoice = enemySel.Next(0, enemyTeam.Length);
             {
-                foeFighter = enemyTeam[0];
-                Console.WriteLine("The opponent sends out " + enemyTeam[0].Name);
+                fighters[1] = enemyTeam[foeChoice];
+                Console.WriteLine("Opponent sent out " + fighters[1].Name);
             }
-            else if (foeChoice == 2)
-            {
-                foeFighter = enemyTeam[1];
-                Console.WriteLine("The opponent sends out " + enemyTeam[1].Name);
-            }
-            else if (foeChoice == 3)
-            {
-                foeFighter = enemyTeam[1];
-                Console.WriteLine("The opponent sends out " + enemyTeam[1].Name);
-            }
+
+
+        }
+
+        public static void Battle()
+        {
+            int score = 0;
+            int[] stats = { fighters[0].Speed,
+                            fighters[0].Attack,
+                            fighters[0].Defense,
+                            fighters[0].SpAttack,
+                            fighters[0].SpDefense
+                           };
+            int foeScore = 0;
+            int[] foeStats = { fighters[1].Speed,
+                            fighters[1].Attack,
+                            fighters[1].Defense,
+                            fighters[1].SpAttack,
+                            fighters[1].SpDefense
+                           };
+            string[] statNames = { "Speed",
+                                   "Attack",
+                                   "Defense",
+                                   "Special Attack",
+                                   "Special Defense"
+                                  };
+
+
 
             Console.WriteLine("The two pokemon battle!");
 
-            int fighterScore = 0;
-            int foeScore = 0;
-
-            //speed
-            if (fighter.Speed >= foeFighter.Speed)
+            for (int i = 0; i <= stats.Length-1; i++)
             {
-                fighterScore++;
-                Console.WriteLine(fighter.Name + " strikes first!");
-            }
-            else
-            {
-                foeScore++;
-                Console.WriteLine(foeFighter.Name + " strikes first!");
-            }
-
-            //attack
-            if (fighter.Attack >= foeFighter.Attack)
-            {
-                fighterScore++;
-                Console.WriteLine(fighter.Name + " deals a heavy blow!");
-            }
-            else
-            {
-                foeScore++;
-                Console.WriteLine(foeFighter.Name + " deals a heavy blow!");
-            }
-
-            //defense
-            if (fighter.Defense >= foeFighter.Defense)
-            {
-                fighterScore++;
-                Console.WriteLine(fighter.Name + " blocks an attack and hits back even harder!");
-            }
-            else
-            {
-                foeScore++;
-                Console.WriteLine(foeFighter.Name + " blocks an attack and hits back even harder!");
-            }
-
-            //spAttack
-            if (fighter.SpAttack >= foeFighter.SpAttack)
-            {
-                fighterScore++;
-                Console.WriteLine(fighter.Name + " blasts it's opponent!");
-            }
-            else
-            {
-                foeScore++;
-                Console.WriteLine(foeFighter.Name + " blasts it's opponent!");
-            }
-
-            //spDefense
-            if (fighter.SpDefense >= foeFighter.SpDefense)
-            {
-                fighterScore++;
-                Console.WriteLine(fighter.Name + " unleashes its opponent's energy back at it!");
-            }
-            else
-            {
-                foeScore++;
-                Console.WriteLine(foeFighter.Name + " unleashes its opponent's energy back at it!");
-            }
-
-            if(fighterScore > foeScore)
-            {
-                Console.WriteLine(fighter.Name + " defeated " + foeFighter.Name + "!");
-            }
-            else
-            {
-                Console.WriteLine(foeFighter.Name + " defeated " + fighter.Name + "!");
-            }
-            Console.ReadKey();
-
-        }
-
-
-
-        public static string ReadFile(string fileName)
-        {
-            using (var reader = new StreamReader(fileName))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-        public static List<Pokemon> ReadPokemon(string fileName)
-        {
-            var pokedex = new List<Pokemon>();
-            using (var reader = new StreamReader(fileName))
-            {
-                string line = "";
-
-                while ((line = reader.ReadLine()) != null)
+                if (stats[i] > foeStats[i])
                 {
-                    var pokemon = new Pokemon();
-                    
-                    string[] values = line.Split(',');
-
-                    pokemon.Name = values[1];
-                    pokemon.Type1 = values[2];
-                    pokemon.Type2 = values[3];
-                    pokemon.Legendary = values[12];
-                    pokemon.Name = values[1];
-                    pokemon.Type1 = values[2];
-                    pokemon.Type2 = values[3];
-                    pokemon.Legendary = values[12];
-
-                    int parseInt;
-
-                    if (int.TryParse(values[0], out parseInt))
-                    {
-                        pokemon.Number = parseInt;
-                    }
-                    if (int.TryParse(values[5], out parseInt))
-                    {
-                        pokemon.Hp = parseInt;
-                    }
-                    if (int.TryParse(values[6], out parseInt))
-                    {
-                        pokemon.Attack = parseInt;
-                    }
-                    if (int.TryParse(values[7], out parseInt))
-                    {
-                        pokemon.Defense = parseInt;
-                    }
-                    if (int.TryParse(values[8], out parseInt))
-                    {
-                        pokemon.SpAttack = parseInt;
-                    }
-                    if (int.TryParse(values[9], out parseInt))
-                    {
-                        pokemon.SpDefense = parseInt;
-                    }
-                    if (int.TryParse(values[10], out parseInt))
-                    {
-                        pokemon.Speed = parseInt;
-                    }
-                    if (int.TryParse(values[11], out parseInt))
-                    {
-                        pokemon.Generation = parseInt;
-                    }
-
-                    pokedex.Add(pokemon);
+                    score++;
+                    Console.WriteLine("Your " + fighters[0].Name + " scores a hit with its superior " + statNames[i] + " stat!");
+                }
+                else if (foeStats[i] > stats[i])
+                {
+                    foeScore++;
+                    Console.WriteLine("The opponent's " + fighters[1].Name + " scores a hit with its superior " + statNames[i] + " stat!");
+                }
+                else
+                {
+                    Console.WriteLine("The Pokemon are equally matched in " + statNames[i] + "!");
                 }
             }
-            return pokedex;
+
+            if (score > foeScore)
+            {
+                Console.WriteLine("Your " + fighters[0].Name + " defeated " + fighters[1].Name +"!");
+            }
+            else if (score < foeScore)
+            {
+                Console.WriteLine("The opponent's " + fighters[1].Name + " defeated " + fighters[0].Name + "!");
+            }
+            else
+            {
+                Console.WriteLine("After an epic clash both pokemon faint!");
+            }
+            
+
         }
-    }  
-}
+
+            public static string ReadFile(string fileName)
+            {
+                using (var reader = new StreamReader(fileName))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+
+            public static List<Pokemon> ReadPokemon(string fileName)
+            {
+                var pokedex = new List<Pokemon>();
+                using (var reader = new StreamReader(fileName))
+                {
+                    string line = "";
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var pokemon = new Pokemon();
+
+                        string[] values = line.Split(',');
+
+                        pokemon.Name = values[1];
+                        pokemon.Type1 = values[2];
+                        pokemon.Type2 = values[3];
+                        pokemon.Legendary = values[12];
+                        pokemon.Name = values[1];
+                        pokemon.Type1 = values[2];
+                        pokemon.Type2 = values[3];
+                        pokemon.Legendary = values[12];
+
+                        int parseInt;
+
+                        if (int.TryParse(values[0], out parseInt))
+                        {
+                            pokemon.Number = parseInt;
+                        }
+                        if (int.TryParse(values[5], out parseInt))
+                        {
+                            pokemon.Hp = parseInt;
+                        }
+                        if (int.TryParse(values[6], out parseInt))
+                        {
+                            pokemon.Attack = parseInt;
+                        }
+                        if (int.TryParse(values[7], out parseInt))
+                        {
+                            pokemon.Defense = parseInt;
+                        }
+                        if (int.TryParse(values[8], out parseInt))
+                        {
+                            pokemon.SpAttack = parseInt;
+                        }
+                        if (int.TryParse(values[9], out parseInt))
+                        {
+                            pokemon.SpDefense = parseInt;
+                        }
+                        if (int.TryParse(values[10], out parseInt))
+                        {
+                            pokemon.Speed = parseInt;
+                        }
+                        if (int.TryParse(values[11], out parseInt))
+                        {
+                            pokemon.Generation = parseInt;
+                        }
+
+                        pokedex.Add(pokemon);
+                    }
+                }
+                return pokedex;
+            }
+        }
+    }
+
